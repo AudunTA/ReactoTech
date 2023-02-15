82519f137b58dce1a0dbd88e2a80d8cab5cb580e
AudunTA <aud-and2@online.no>
2023-02-15 16:02:09 +0100

    created react vite app

---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Card from "./components/Card";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-vue-app/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="card">
+      <h1>Bose Quiet Comfort 45</h1>
+      <p>
+        Experience crystal-clear sound with our noise-cancelling headphones -
+        perfect for any setting.
+      </p>
+      <button className="sbtn-addToCart">Add to cart</button>
+    </div>
+  );
+}
+
+export default Card;
