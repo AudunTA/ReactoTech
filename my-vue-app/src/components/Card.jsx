a48a2ff8d1a4342a5eb806f7f1590c7f9502c54f
AudunTA <aud-and2@online.no>
2023-02-15 18:24:56 +0100

    added card for testing

--- my-vue-app/src/components/Card.jsx
+++ my-vue-app/src/components/Card.jsx
@@ -3,12 +3,19 @@ import React from "react";
 function Card() {
   return (
     <div className="card">
+      <div className="wrapper-img">
+        <img
+          src="src\images\—Pngtree—headset black music_3743151.png"
+          className="img-card"
+        ></img>
+      </div>
+
       <h1>Bose Quiet Comfort 45</h1>
       <p>
         Experience crystal-clear sound with our noise-cancelling headphones -
         perfect for any setting.
       </p>
-      <button className="sbtn-addToCart">Add to cart</button>
+      <button className="btn-addToCart">Add to cart</button>
     </div>
   );
 }
